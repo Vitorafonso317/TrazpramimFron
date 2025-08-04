af2015fc97448a9571fcde76b8a6c0d30493e587
Vitorafonso317 <1067469@senacminas.edu.br>
2025-08-04 14:17:16 -0300

    update

--- src/pages/suporte.jsx
+++ src/pages/suporte.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
 import SuporteForm from "../components/suporteForm";
+import RegisterPage from "./registerPage";
 
  
 const Suporte = () => {
